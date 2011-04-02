255e61e8d30df54930d676583899aba118a3e3e1
Matt Jones <biz@mhjones.org>
2011-04-02 12:07:40 -0700

    stub header

---
+++ src/mobitrie.h
@@ -0,0 +1,22 @@
+#ifndef _MOBITRIE_H_
+#define _MOBITRIE_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint64_t offset;
+
+typedef struct { } MTrie;
+typedef struct { } MTrieIter;
+typedef struct { } KeyOffset;
+
+void mt_set_key(MTrie *mt, char *key, offset val);
+
+bool mt_contains_key(MTrie *mt, char *key);
+int mt_count_prefix(MTrie *mt, char *prefix);
+
+MTrieIter* mt_iter_start(MTrie *mt, char *prefix);
+KeyOffset* mt_iter_next(MTrieIter *iter);
+void mt_iter_free(MTrieIter *iter);
+
+#endif /* _MOBITRIE_H_ */
